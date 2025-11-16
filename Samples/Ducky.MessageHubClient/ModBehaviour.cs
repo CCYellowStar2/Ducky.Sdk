26bd1ef5416ea9ad07a473916e63f39dfd9e5eb5
Newbe36524 <newbe36524@qq.com>
2025-11-16 02:01:23 -0500

    ModHttpV1 & Terminal test (#18)
    Co-authored-by: ReSharper CLI CleanupCode GitHub Action <niceformate@nice.com>

---
+++ Samples/Ducky.MessageHubClient/ModBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.CommandLine.Parsing;
+using Cysharp.Threading.Tasks;
+using Ducky.Sdk.Contracts;
+using Ducky.Sdk.Contracts.CommandLine;
+using Ducky.Sdk.Logging;
+using Ducky.Sdk.ModBehaviours;
+
+namespace Ducky.MessageHubClient;
+
+public class ModBehaviour : ModBehaviourBase
+{
+    protected override void ModEnabled()
+    {
+        var modRootCommand = new ModRootCommand("Ducky Message Hub Client");
+        // 使用 ModTerminalClientContract 连接
+        UniTask.RunOnThreadPool(async () =>
+        {
+            var client = Contract.ModTerminalClient;
+            await client.Connect(async (terminal, id, message, toTerminal) =>
+            {
+                Log.Info($"Received message from {id}: {message}");
+                if (message == "ping")
+                {
+                    await toTerminal("pong");
+                }
+
+                else
+                {
+                    var parseResult = CommandLineParser.Parse(modRootCommand, message);
+                    await parseResult.InvokeAsync();
+                }
+            });
+        });
+    }
+
+    protected override void ModDisabled()
+    {
+    }
+}
